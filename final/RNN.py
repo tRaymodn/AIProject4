83d77acb1a00a9a96bfb914455b154c98120f6d8
Tucker Raymond <teraymond@wpi.edu>
2022-12-16 15:24:00 -0500

    Add prediction on testing data

--- final/RNN.py
+++ final/RNN.py
@@ -19,50 +19,29 @@ def make_model():
     games = getAllGames()
     games = train_test_split(games, test_size=0.2, random_state=40, shuffle=False)
     teams_seen = []  # List of teams that have been reached in the loop
-    sequence_labels = []  # List of true labels for each data point
     game_sequences = []  # Nested list of data for each team for every game they've played
     for game in games[0][:200]:
         if game[1] not in teams_seen and len(game[1]) == 3:  # If the team hasn't been seen yet, add it in the seen teams and game sequences
             teams_seen.append(game[1])
             game_sequences.append(getTeamData(game[1], game[0]))
-            sequence_labels.append(game[3])
         elif len(game[1]) == 3:  # Adds game values if team has been seen already
             game_sequences[teams_seen.index(game[1])].append(getTeamData(game[1], game[0]))
-            sequence_labels.append(game[3])
 
         if game[2] not in teams_seen and len(game[2]) == 3:
             teams_seen.append(game[2])
             game_sequences.append(getTeamData(game[2], game[0]))
-            sequence_labels.append(game[3])
         elif len(game[2]) == 3:
             game_sequences[teams_seen.index(game[2])].append(getTeamData(game[2], game[0]))
-            sequence_labels.append(game[3])
     print("Shape of game_sequences: ", np.shape(game_sequences))
 
-    seq_num = 0
-    labs = []
     arr = []
-    """for seq in game_sequences:  # Runs through the 32 sequences of games for each team
-        seq.pop(0)  # The first value in the sequence will always be empty because it is the first game the team played
-        print("Sequence shape: ", np.shape(seq))
-        for i in range(0, len(seq)):  # Loop through game values for a certain team
-            print("game shape: ", np.shape(seq[i]))
-            if i % 4 == 0:
-                arr.append(seq[i])
-                print("first game: ", seq[i])
-            elif i % 4 == 1:
-                # print("Shape of arr[len(arr) - 1]: ", np.shape(arr[len(arr) - 1]), "\ngame shape: ", np.shape(seq[i]))
-                arr[len(arr) - 1] = np.stack((arr[len(arr) - 1], seq[i]))
-            else:
-                arr[len(arr) - 1] = np.concatenate((arr[len(arr) - 1], [seq[i]]))"""
     sequence_labels = []
     for seq in game_sequences:
         seq.pop(0)
-        print("length of sequence: ", len(seq))
+        print("length of training sequence: ", len(seq))
         print("seq: ", seq)
         i = 0
         sequence_clipped = []  # Array where the short sequences will be stored
-        labels = []
         while i < 8:  # Creating two sequences of four games each from each team's full sequence of games
             if i == 4:
                 arr.append(np.array(sequence_clipped))  # Add short sequence to the final array
@@ -102,6 +81,55 @@ def make_model():
     model.compile(optimizer=opt, loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy', 'mse'])
 
     print(np.shape(arr))
-    model.fit(arr, labs, epochs=200)
+    model.fit(arr, labs, epochs=400)  # Fit the training data to the RNN model and run for 400 epochs
+
+    # Put the testing data into game sequences by team
+    testing_teams_seen = []
+    testing_sequences = []
+    for game in games[1][:70]:
+        if game[1] not in testing_teams_seen and len(game[1]) == 3:  # If the team hasn't been seen yet, add it in the seen teams and game sequences
+            testing_teams_seen.append(game[1])
+            testing_sequences.append([getTeamData(game[1], game[0])])
+        elif len(game[1]) == 3:  # Adds game values if team has been seen already
+            testing_sequences[testing_teams_seen.index(game[1])].append(getTeamData(game[1], game[0]))
+
+        if game[2] not in testing_teams_seen and len(game[2]) == 3:
+            testing_teams_seen.append(game[2])
+            testing_sequences.append([getTeamData(game[2], game[0])])
+        elif len(game[2]) == 3:
+            testing_sequences[testing_teams_seen.index(game[2])].append(getTeamData(game[2], game[0]))
+    print("Shape of testing sequences: ", np.shape(testing_sequences))
+
+    testing_arr = []
+    testing_labels = []
+    for seq in testing_sequences:
+        print("length of testing sequence: ", len(seq))
+        print("seq: ", seq)
+        testing_arr.append(np.array(seq[0:2]))
+        if seq[1][48] > seq[1][49]:
+            testing_labels.append([0])
+        else:
+            testing_labels.append([1])
+        
+    print("Shape of testing array: ", np.shape(testing_arr))
+
+    y_pred = model.predict(np.array(testing_arr))
+    
+    print("Testing true labels: ", testing_labels)
+    print("Testing predicted labels: ", y_pred)
+    missed_vals = 0
+    for index in range(0, len(y_pred)):
+        if y_pred[index] > 0.5:
+            y_pred[index] = 1
+        else:
+            y_pred[index] = 0
+    points_mislabeled = 0
+    for ind in range(0, len(testing_labels)):
+        if testing_labels[ind] != y_pred[ind]:
+            points_mislabeled += 1
+
+    print("Number of games predicted incorrectly: ", points_mislabeled)
+    print("Total points: ", len(testing_labels))
+
 
 make_model()
